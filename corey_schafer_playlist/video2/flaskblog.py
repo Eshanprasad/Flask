f0e5eb8a9ea1c89619d1d3f853d985262c618297
eshan_prasad <20d45a0205.eshan@gmail.com>
2024-10-07 17:51:04 +0530

    video2 templates

---
+++ corey_schafer_playlist/video2/flaskblog.py
@@ -0,0 +1,36 @@
+from flask import Flask, render_template
+
+app = Flask(__name__)
+
+# assume the below list is obtained from database
+posts_list = [
+    {
+        'author': 'Corey Schafer',
+        'title': 'Blog Post 1',
+        'content': 'First post content',
+        'date_posted': 'April 20, 2018'
+    },
+    {
+        'author': 'Jane Doe',
+        'title': 'Blog Post 2',
+        'content': 'Second post content',
+        'date_posted': 'April 21, 2018'
+    }
+]
+
+#routes are what we type into our browser to go to different pages.
+# can have multiple routes to a same function
+#This home function will be executed and displays "home.html" page in default url ("/") as well as in ("/home") url
+@app.route('/')
+@app.route('/home')
+def home():
+    return render_template("home.html", posts=posts_list)    #we are not passing "title" variable to home.html, therefore there is no title variable
+
+# "posts" is a variable which will be used in home.html, to get the data from "post_list" to html page.
+
+@app.route('/about')
+def about():
+    return render_template("about.html")
+
+if __name__=="__main__":
+    app.run(debug=True)
