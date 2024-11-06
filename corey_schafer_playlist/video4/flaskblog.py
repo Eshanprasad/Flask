efa72ce7f432ea89458a7c8d8b7ea637d9e625ed
eshan_prasad <20d45a0205.eshan@gmail.com>
2024-11-06 13:23:31 +0530

    database added and commands explained in notes.txt

--- corey_schafer_playlist/video4/flaskblog.py
+++ corey_schafer_playlist/video4/flaskblog.py
@@ -1,9 +1,35 @@
 from flask import Flask, render_template, url_for, flash, redirect
+from flask_sqlalchemy import SQLAlchemy
 from forms import RegistrationForm, LoginForm
+from datetime import datetime
 
 app = Flask(__name__)
-
 app.config['SECRET_KEY'] = "b07294eeda19942fab7d322ac480c602"            # the SECRET_KEY is used to protect against Cross-Site Request Forgery (CSRF) attacks
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///site.db'
+db = SQLAlchemy(app)
+
+class User(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    username = db.Column(db.String(50), unique=True, nullable=False)    # String max length 50
+    email = db.Column(db.String(120), unique=True, nullable=False)
+    image_file = db.Column(db.String(20), nullable=False, default='default.jpeg')    #adding default image
+    password = db.Column(db.String(60), nullable=False)
+    posts = db.relationship('Post', backref='author', lazy=True)            #posts has a relationship with Post class. What backref allows us to do is when we have a post, we can simply use this author attribute to get to the user who created the post
+                             #here Post is class name.                      # Lazy defines when the SQLAlchemy loads the data from database.True means it loads the data in one go
+
+    def __repr__(self):             # print's object as described in this method
+        return f"User('{self.username}', '{self.email}', '{self.image_file}')"
+
+
+class Post(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    title = db.Column(db.String(100), nullable=False)
+    date_posted=db.Column(db.DateTime, nullable=False, default=datetime.utcnow)
+    content=db.Column(db.Text, nullable=False)
+    user_id = db.Column(db.Integer, db.ForeignKey('user.id'), nullable=False)           # this is primary key of User class.
+                                                 #in user.id u is lowercase, but User class has Uppercase U. This is because here we are referring to table name not the class name. Class name is tabe name but all in lower case. 
+    def __repr__(self):             # print's object as described in this method
+        return f"Post('{self.title}', '{self.date_posted}', '{self.content}')"
 
 # assume the below list is obtained from database
 posts_list = [
